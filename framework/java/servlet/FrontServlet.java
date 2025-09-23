e78207a5a146a49da81497804a71389183c1110f
Lapatia48 <ravelojaona48@gmail.com>
2025-09-23 23:19:43 +0300

    finalisation url(sprint1)

--- framework/java/servlet/FrontServlet.java
+++ framework/java/servlet/FrontServlet.java
@@ -5,19 +5,25 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import jakarta.servlet.RequestDispatcher;
 
 public class FrontServlet extends HttpServlet {
-    
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        
-        // On pointe vers la vue JSP
-        RequestDispatcher dispatcher = req.getRequestDispatcher("/views/index.jsp");
-        
-        // On transfère la requête/réponse vers la JSP
+
+        String requestedUrl = req.getRequestURI().substring(req.getContextPath().length());
+        req.setAttribute("url", requestedUrl);
+
+        // Forward officiel vers JSP sous WEB-INF
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/index.jsp");
         dispatcher.forward(req, resp);
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        doGet(req, resp); // Traite POST comme GET
     }
 }
---
+++ test/WEB-INF/views/index.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>Front Controller Test</title>
+</head>
+<body>
+    <p><b>${url}</b></p>
+</body>
+</html>
