e989a04a07b15d34b11764466c5b2616a66d3c7c
Lapatia48 <ravelojaona48@gmail.com>
2025-10-17 20:29:31 +0300

    url+checher dans ressource

--- framework/java/servlet/FrontServlet.java
+++ framework/java/servlet/FrontServlet.java
@@ -9,21 +9,29 @@ import jakarta.servlet.RequestDispatcher;
 
 public class FrontServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-
-        String requestedUrl = req.getRequestURI().substring(req.getContextPath().length());
-        req.setAttribute("url", requestedUrl);
-
-        // Forward officiel vers JSP sous WEB-INF
-        RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/index.jsp");
-        dispatcher.forward(req, resp);
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        chercherRessource(req, resp);
     }
 
-
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        doGet(req, resp); // Traite POST comme GET
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        chercherRessource(req, resp);
+    }
+
+    private void chercherRessource(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+        String path = req.getRequestURI().substring(req.getContextPath().length());
+        if ("/".equals(path)) {
+            resp.getWriter().println("/");
+            return;
+        }
+
+        boolean resourceExists = getServletContext().getResource(path) != null;
+        if (resourceExists) {
+            RequestDispatcher defaultDispatcher = getServletContext().getNamedDispatcher("default");
+            defaultDispatcher.forward(req, resp);
+        } else {
+            resp.getWriter().println(path);
+        }
     }
 }
